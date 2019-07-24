c419136a408bb672f7a3a2f22a89ac9a4147f8c5
hallavo <32930125+hallavo@users.noreply.github.com>
2019-07-24 13:59:02 +0300

    Added some comments etc

--- convert/convert.py
+++ convert/convert.py
@@ -1,45 +1,40 @@
 import json, sys, codecs, copy
 
+# 2017-07-24
+# A script that takes a json file and outputs a modified json file
+# (changes the structure and replaces null values with proper strings)
+
 # sys.argv[0]   --   name of the python script
 # sys.argv[1]   --   arg1
 # sys.argv[2]   --   arg2
-
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
-
-# read file
+# read json file
 with codecs.open(inputFile, 'r', encoding='utf8') as infile:
     data=infile.read()
 
-# parse file
+# load json into a dict
 dump = json.loads(data)
-voucherDict = dump["vourcherList"] # Note: vou_R_cher
+voucherDict = dump["vourcherList"] # Note: vou_R_cher (typo)
 
+# create a modified json string
 outJsonString = '{"vouchers":['
 for voucher in voucherDict:
     lineList = voucher["voucherLines"]
     for transactionDict in lineList:
         newVoucher = copy.deepcopy( {**transactionDict, **voucher} )
         del newVoucher["voucherLines"]
-        #del newVoucher["reference"]
-        #del newVoucher["addressid"]
-
         json_string = json.dumps(newVoucher)
         outJsonString += json_string
         outJsonString += ","
-        #print(json_string)
-        #print(newVoucher)
-        #print('\n')
-
 outJsonString += " {}]}"
-outJsonString = outJsonString.replace(' null',' "thenullval"')
+outJsonString = outJsonString.replace(' null',' "thenullval"') #replace null values
+
+# create a new dict
 outJsonDict = json.loads(outJsonString)    
-#obj2 = json.dumps( obj["vourcherList"] )
-#print(obj["vourcherList"])
 
-
-# write file
+# write json file
 with codecs.open(outputFile, 'w', encoding='utf8') as outfile:
     data = json.dumps(outJsonDict, ensure_ascii=False)
     outfile.write(data)
