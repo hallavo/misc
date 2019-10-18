2c900f351889679d4067f22354c499c487dd56f5
hallavo <32930125+hallavo@users.noreply.github.com>
2019-10-18 15:04:15 +0300

    added problem description for Pi-Golf

---
+++ pi-golf.py
@@ -0,0 +1,15 @@
+"""
+Pi-Golf
+Mystery of mysteries, icon of geekdom... the irrational number pi. Is it a secret code, a message from the universe? Let's find out.
+
+Convert the digits of pi into uppercase ASCII letters.
+
+What is the first 3-letter English word in pi? (Hint: The word 'OLD' appears at digit 1807, and it is not the first one.
+What is the first 4-letter word? (Hint: it's a weird part of a bird!)
+What is the first 5-letter word? (Hint: 'VIRUS' is at 3056055 and it's not the first.)
+6-letter word? (It is suuuuch a good word! Hint: Google would have chosen it if somehow they controlled the universe itself which they don't, do they?)
+7-letter word...? This is an open question. I have not found it yet!
+You can find millions of digits of pi online, but I also have txt files of pi up to a billion digits.
+
+source: http://puzzles.bostonpython.com/pigolf.html
+"""
