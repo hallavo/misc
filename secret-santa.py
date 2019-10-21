2b8a7760d925b0d581224a1d88cdb07bcc3aab4d
hallavo <32930125+hallavo@users.noreply.github.com>
2019-10-21 15:56:56 +0300

    Secret Santa: assign a group of people designated gift recipients

---
+++ secret-santa.py
@@ -0,0 +1,47 @@
+#
+# Secret Santa
+#    Given N people, assign each person a 'designated gift recipient'(TM).
+#    - everyone should receive exactly one gift
+#    - no one should be their own designated gift recipient
+#
+# (a.k.a. Generate a random cycle of length N)
+#
+
+import itertools
+import random
+import pprint
+
+
+class Person:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+def getGiftRecipients(names: tuple) -> dict:
+    p = list( itertools.permutations(names) )
+    cycle = random.choice(p)
+    
+    x_gives_to_y = {}
+    for i in range(len(names)):
+        x_gives_to_y[ cycle[i-1].email ] = cycle[i].name
+
+    return x_gives_to_y
+
+
+def main():
+    people = (
+                Person('Ixxxx',  'xxxx@gmail.com'),
+                Person('Mxxxx',  'xxxx@gmail.com'),
+                Person('Vxxxx',  'xxxx@gmail.com'),
+                Person('Axxxx',  'xxxx@gmail.com'),
+                Person('Lxxxx',  'xxxx@gmail.com')
+             )
+    recipients = getGiftRecipients(people)
+    pprint.pprint(recipients)
+    
+
+main()
+
+# TO DO:
+#   inform everyone of their designated gift recipient by email
